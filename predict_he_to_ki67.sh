c43972f88b1872f87899b3a3523e0a4eb4f01fb7
phiwei@mac <philippe.weitz@ki.se>
2021-01-11 18:46:44 +0100

    fit he to ki67 GAN on gpu 1

--- predict_he_to_ki67.sh
+++ predict_he_to_ki67.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python predict_WSI.py --dataroot ./ --name WSI_GAN_he_to_ki67 --model cycle_gan --dataset_mode WSIPrediction --no_dropout --model test --direction BtoA
+python predict_WSI.py --dataroot ./ --name WSI_GAN_he_to_ki67 --model cycle_gan --dataset_mode WSIPrediction --no_dropout --model test --direction BtoA --gpu_ids 1
